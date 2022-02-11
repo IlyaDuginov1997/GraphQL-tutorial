cb3634b57514a24f0eabc77b20db622a273f8c6d
Ilya <IlyaDuginov1997@gmail.com>
2022-02-11 23:40:34 +0300

    Mutation in GraphQL

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
-const schema = require('../schema/schema')
-const mongoose = require('mongoose')
+const schema = require('../schema/schema');
+const mongoose = require('mongoose');
 
 
 const app = express();
@@ -9,16 +9,17 @@ const PORT = 3005;
 
 mongoose.connect(
     `mongodb+srv://Ilya:ilya123@ilyaclaster.y7spn.mongodb.net/GraphQL-tutorial?retryWrites=true&w=majority`,
-)
+    {useNewUrlParser: true}
+);
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true,
 }));
 
-const dbConnection = mongoose.connection
-dbConnection.on('error', err => console.log(`Connection error: ${err}`))
-dbConnection.once('open', () => console.log(`Connected to DB`) )
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log(`Connected to DB`));
 
 app.listen(PORT, err => {
     err ? console.log(err) : console.log('Server started!');
